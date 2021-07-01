7f408b0751579c468ee346eda397f8a8a1b3dac2
pearl2134 <86753599+pearl2134@users.noreply.github.com>
2021-07-01 08:34:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Gọi cậu là sông Bạch Đằng
+    Vì đã được tớ  đóng cọc năm 938 :3`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Đấm hay yêu :) 😙',
+    desc: 'hỏi nhẹ thôi đừng áp lực :) yêu hay đấm ',
+    btnYes: 'Vẫn cứ là thích bae <33',
+    btnNo: 'cho xin cái hẹn :)))) ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cưng lại iu iem <3',
+    btnReply: 'Gửi cho me <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'chị biết mà :33. Yêu em nhiều nhiều 😘😘',
+    messDesc: 't thèm trà đào :)chiều mua nhe :).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
